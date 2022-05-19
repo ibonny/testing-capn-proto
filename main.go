8ee52b8d83e64757c51fbbeea5d33d3536fd34ce
ibonny <ibonny@gmail.com>
2022-05-19 20:01:00 +0000

    Saving.

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"fmt"
 	"testing_capn_proto/schemas"
 
 	"capnproto.org/go/capnp/v3"
@@ -19,11 +20,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	book.SetTitle("War and Peace")
 	book.SetPageCount(1440)
 
+	var buff bytes.Buffer
+
 	// Write the message to stdout.
-	if err = capnp.NewEncoder(os.Stdout).Encode(msg); err != nil {
+	if err = capnp.NewEncoder(&buff).Encode(msg); err != nil {
 		panic(err)
 	}
+
+	var msg2 *capnp.Message
+
+	// Read the message from stdin.
+	if msg2, err = capnp.NewDecoder().Decode(); err != nil {
+		panic(err)
+	}
+
+	// Extract the root struct from the message.
+	book, err := books.ReadRootBook(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Access fields from the struct.
+	title, err := book.Title()
+	if err != nil {
+		panic(err)
+	}
+	pageCount := book.PageCount()
+	fmt.Printf("%q has %d pages\n", title, pageCount)
 }
